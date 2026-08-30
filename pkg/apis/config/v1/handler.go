ff44625c5b3ebafcf2c0cb0171a6bb31f69a6b31
lixd <xueduan.li@gmail.com>
2026-08-30 15:11:02 +0800

    fix(config): use secure web terminal RSA keys

--- pkg/apis/config/v1/handler.go
+++ pkg/apis/config/v1/handler.go
@@ -273,7 +273,7 @@ func (h *handler) CreateSSHRSAKey(req *restful.Request, resp *restful.Response) 
 		restplus.HandleInternalError(resp, req, err)
 		return
 	}
-	t.PrivateKey, t.PublicKey, err = certs.GetSSHKeyPair(512)
+	t.PrivateKey, t.PublicKey, err = certs.GetSSHKeyPair(certs.DefaultRSAKeySize)
 	if err != nil {
 		restplus.HandleInternalError(resp, req, err)
 		return
@@ -288,7 +288,7 @@ func (h *handler) CreateSSHRSAKey(req *restful.Request, resp *restful.Response) 
 }
 
 func generateWebTerminal() (v1.WebTerminal, error) {
-	priv, pub, err := certs.GetSSHKeyPair(512)
+	priv, pub, err := certs.GetSSHKeyPair(certs.DefaultRSAKeySize)
 	if err != nil {
 		return v1.WebTerminal{}, err
 	}
